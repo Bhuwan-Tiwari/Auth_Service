39b7bdd46fcb9eb4a745c9677720d817c95c111b
Bhuwan Tiwari <bhuwan1200@gmail.com>
2024-01-14 20:37:16 +0530

    Added funtion to compare the incoming password with encrypted password

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,44 +1,53 @@
- const jwt = require('jsonwebtoken')
- const UserRepository = require('../repository/user-repository');
- const { JWT_KEY}= require('../config/serverconfig')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
-class UserService{
-    constructor(){
+const UserRepository = require('../repository/user-repository');
+const { JWT_KEY } = require('../config/serverconfig')
+
+
+class UserService {
+    constructor() {
         this.userRepository = new UserRepository();
     }
 
-    async create (data)
-    {
+    async create(data) {
         try {
             const user = await this.userRepository.create(data);
             return user;
         } catch (error) {
-         console.log("something went wrong in service layer")
-         throw error;   
+            console.log("something went wrong in service layer")
+            throw error;
         }
     }
 
-    createToken(user)
-    {
+    createToken(user) {
         try {
-            const result = jwt.sign(user,JWT_KEY,{expiresIn : '1d'})
+            const result = jwt.sign(user, JWT_KEY, { expiresIn: '1d' })
             return result;
         } catch (error) {
             console.log('Something went wrong in token creation')
             throw error;
-            
+
         }
     }
 
-      verifyToken(token)
-      {
+    verifyToken(token) {
         try {
-            const response = jwt.verify(token,JWT_KEY)
+            const response = jwt.verify(token, JWT_KEY)
             return response;
         } catch (error) {
             console.log('somethinf went wroong in service layer')
             throw error;
         }
-      }
-} 
+    }
+
+    checkPassword(userPlainPassword, encryptedPassword) {
+     try {
+        return bcrypt.compareSync(userPlainPassword,encryptedPassword)
+     } catch (error) {
+        console.log('somethinf went wroong in service layer')
+        throw error;
+     }
+    }
+}
 module.exports = UserService
