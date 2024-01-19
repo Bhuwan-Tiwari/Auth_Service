ab758488d142253e34ce288e21e95775ba491eef
Bhuwan Tiwari <bhuwan1200@gmail.com>
2024-01-19 13:32:48 +0530

    Added email and password validation for imcomming request

---
+++ src/middlewares/auth-request-validators.js
@@ -0,0 +1,17 @@
+const validateUserAuth =(req,res,next)=>
+{
+    if(!req.body.email || !req.body.password)
+    {
+        return res.status(400).json({
+            success : false,
+            data:{},
+            message : 'something went wrong',
+            err :'Email or password missing in the singup request'
+        })
+    }
+
+    next();
+}
+module.exports ={
+    validateUserAuth
+}
